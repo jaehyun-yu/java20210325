ae2cb45638bd09b1c178b8e486de71a598de8d5f
jaehyun-Yu <yjh454@naver.com>
2021-03-25 10:43:01 +0900

    ..

--- src/p01/Comment.java
+++ src/p01/Comment.java
@@ -4,7 +4,7 @@ public class Comment {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("주석");
-		System.out.println("한 줄 주석 : 슬래시 두개");
-		System.out.println("여러줄주석 : /* */");
+		System.out.println("한 줄 주석 : //");
+		System.out.println("여러줄주석 : /** */");
 	}
 }
