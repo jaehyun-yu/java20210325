26b4843a5cba4cf1b995c767e3a808c75c6b35d2
jaehyun-Yu <yjh454@naver.com>
2021-03-30 16:52:01 +0900

    2021-03-30/16:51

---
+++ src/p03/textbook/exercise/Exercise01.java
@@ -0,0 +1,45 @@
+package p03.textbook.exercise;
+
+public class Exercise01 {
+	public static void main(String[] args) {
+//	int x=10;
+//	int y=20;
+//	int z= (++x) + (y--);
+//	System.out.println(z);
+//		int score = 85;
+//		String result = (!(score>90))? "가":"나";
+//		System.out.println(result);
+//		int pencilsPerStudent = (534/30);
+//		System.out.println(pencilsPerStudent);
+//		
+//		//남은 연필수
+//		int pencilsLeft = (534 % 30);
+//		System.out.println(pencilsLeft);
+	
+//		int lengthTop = 5;
+//		int lengthBottom = 10;
+//		int height = 7;
+//		double area = (double)((lengthTop + lengthBottom) * height );
+//		System.out.println(area);
+		
+//		int x = 10;
+//		int y =5;
+//		
+//		System.out.println((x>7) && (y<=5));
+//		System.out.println((x%3 == 2)|| (y%2 != 1));
+		
+//		double x =5.0;
+//		double y =0.0;
+//		
+//		double z = x % y;
+//		if(Double.isNaN(z)) {
+//			System.out.println("0.0으로 나눌수 없습니다");
+//			
+//		} else {
+//			double result =z+10;
+//			System.out.println("결과 "+ result
+//					);
+//		}
+		
+}
+}
