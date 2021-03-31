0e0991a548e61b5a0ff0c35a3eb0778dbd006e05
jaehyun-Yu <yjh454@naver.com>
2021-03-31 17:00:22 +0900

    0331

--- src/p99/programmers/subak.java
+++ src/p99/programmers/subak.java
@@ -3,19 +3,17 @@ package p99.programmers;
 public class subak {
 
 	public static void main(String[] args) {
-		String answer = "";
-	       
-        int n = 0;
-		for (int i=0; i<n; i++){
-            if((i&2) == 0){
-            	answer +=("수");
-            }else {
-                if((i&2) == 1){
-                	answer +=("박");
-                }
-               
-            }
-            return;
+//		String answer = "";
+//	       
+//        int n = 0;
+//		for (int i=0; i<n; i++){
+//            if((i&2) == 0){
+//            	answer +=("수");
+//            }else
+//               answer +=("박");
+//                }
+//               
+//            
+//            return answer;
         }
     }
-}
